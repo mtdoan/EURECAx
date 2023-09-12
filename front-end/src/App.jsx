6e32baecb18653754cb04c5e6a93357bcac9f5d1
ahmyk9 <ahmedkhilji99@gmail.com>
2023-09-12 14:25:16 +1000

    trying

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { HashRouter as Router, Route, Routes } from "react-router-dom"
 
+
+
 import SignIn from './components/auth/SignIn';
 import Dashboard from './components/dashboard/Dashboard';
 
