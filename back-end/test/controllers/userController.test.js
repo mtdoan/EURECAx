d09f8b58f9700112b748e79a677a5d2a859029f5
Mindy Doan <mtdoan0109@gmail.com>
2023-09-19 22:34:57 +1000

    test cases

--- back-end/test/controllers/userController.test.js
+++ back-end/test/controllers/userController.test.js
@@ -4,76 +4,127 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { describe, it } = require('mocha');
 const { app } = require('../../index');
-
-const should = chai.should();
+const Users = require('../../models/userModel');
 
 chai.use(chaiHttp);
-
-// Agent that will keep track of our cookies
+const expect = chai.expect;
 const agent = chai.request.agent(app);
 
-const User = require('../../models/userModel');
+describe('User Controller', () => {
+  beforeEach(async () => {
+    await Users.deleteMany();
+  });
 
-describe('User Controller', function () {
-  // registerUser - return status 400
-  it('Register a new user - User already exists', function (done) {
+  // registerUser - return status 201
+  it('Register a new user - Successful', async () => {
     const reqBody = {
-      username: 'testuser3',
+      username: 'testuser',
       firstname: 'Firstname',
       lastname: 'Lastname',
-      email: 'testemail3@gmail.com',
+      email: 'testemail@gmail.com',
       password: '123456789',
     };
-    agent
-      .post('/api/users')
-      .send(reqBody)
-      .end(function (err, res) {
-        res.should.have.status(400);
-        done();
-      });
+    //
+    const res = await agent.post('/api/users').send(reqBody);
+    expect(res).to.have.status(201);
+  });
+
+  // registerUser - return status 500
+  it('Register a new user - Duplicate key username', async () => {
+    await registerTestUser();
+
+    // re-register a user with existing username
+    const reqBodyUser = {
+      username: 'testuser',
+      firstname: 'Firstname',
+      lastname: 'Lastname',
+      email: 'newtestuser@gmail.com',
+      password: '123456789',
+    };
+    const res = await agent.post('/api/users').send(reqBodyUser);
+    expect(res).to.have.status(500);
+  });
+
+  // registerUser - return status 400
+  it('Register a new user - User email already exists', async () => {
+    await registerTestUser();
+
+    // re-register a user with existing username
+    const reqBodyUser = {
+      username: 'newtestuser',
+      firstname: 'Firstname',
+      lastname: 'Lastname',
+      email: 'testemail@gmail.com',
+      password: '123456789',
+    };
+    const res = await agent.post('/api/users').send(reqBodyUser);
+    expect(res).to.have.status(400);
   });
 
   // authUser - return status 200
-  it('Auth user & get token - Successful', function (done) {
-    const reqBody = {
-      email: 'testemail3@gmail.com',
-      password: '123456789',
-    };
-    agent
-      .post('/api/users/auth')
-      .send(reqBody)
-      .end(function (err, res) {
-        res.should.have.status(200);
-        done();
-      });
+  it('Auth user - Successful', async () => {
+    await registerTestUser();
+    const res = await loginTestUser();
+    expect(res).to.have.status(200);
   });
 
   // authUser - return status 401
-  it('Auth user & get token - Invalid email or password', function (done) {
+  it('Auth user - Invalid email', async () => {
+    await registerTestUser();
+
+    // Auth user with invalid email
     const reqBody = {
       email: 'invalidemail@gmail.com',
-      password: 'invalid',
+      password: '123456789',
     };
-    agent
-      .post('/api/users/auth')
-      .send(reqBody)
-      .end(function (err, res) {
-        res.should.have.status(401);
-        if (err) return done(err);
-        return done();
-      });
+    const res = await agent.post('/api/users/auth').send(reqBody);
+    expect(res).to.have.status(401);
+  });
+
+  // authUser - return status 401
+  it('Auth user - Invalid password', async () => {
+    await registerTestUser();
+
+    // Auth user with invalid password
+    const reqBody = {
+      email: 'testemail@gmail.com',
+      password: 'invalidpassword',
+    };
+    const res = await agent.post('/api/users/auth').send(reqBody);
+    expect(res).to.have.status(401);
+  });
+
+  // Logout - return status 200
+  it('Logout user - Successful', async () => {
+    await registerTestUser();
+    const cookies = (await loginTestUser()).headers['set-cookie'];
+    const res = await agent
+      .post('/api/users/logout')
+      .set('Cookie', cookies)
+      .send();
+    expect(res).to.have.status(200);
   });
 });
 
-describe('Task', (done) => {
-  console.log('mindy delete');
-  afterEach(() => {
-    Task.Users.deleteMany({}, (err, response) => {
-      done();
-    });
-  });
-});
-
-after(function () {
+after(() => {
   agent.close();
 });
+
+const registerTestUser = () => {
+  const reqBody = {
+    username: 'testuser',
+    firstname: 'Firstname',
+    lastname: 'Lastname',
+    email: 'testemail@gmail.com',
+    password: '123456789',
+  };
+  return agent.post('/api/users').send(reqBody);
+};
+
+const loginTestUser = () => {
+  const reqBody = {
+    email: 'testemail@gmail.com',
+    password: '123456789',
+  };
+  return agent.post('/api/users/auth').send(reqBody);
+};
