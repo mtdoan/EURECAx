cbdb2c2d3343a1477a2199240aa99d03e3f6b67c
Mindy Doan <mtdoan0109@gmail.com>
2023-09-19 23:08:20 +1000

    reformat

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -6,197 +6,197 @@ const User = require('../models/userModel');
 // @route POST /api/users/auth
 // @access public
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
-    generateToken(res, user._id);
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
 
-    res.json({
-      _id: user._id,
-      username: user.username,
-      firstname: user.firstname,
-      lastname: user.lastname,
-      email: user.email,
-    });
-  } else {
-    res.status(401);
-    throw new Error('Invalid email or password');
-  }
+        res.json({
+            _id: user._id,
+            username: user.username,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            email: user.email,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 // @desc Register a new user
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { username, firstname, lastname, email, password } = req.body;
+    const { username, firstname, lastname, email, password } = req.body;
 
-  const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email });
 
-  if (userExists) {
-    res.status(400);
-    throw new Error('User already exists');
-  }
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
 
-  const user = await User.create({
-    username,
-    firstname,
-    lastname,
-    email,
-    password,
-  });
-
-  user.save();
-
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      username: user.username,
-      firstname: user.firstname,
-      lastname: user.lastname,
-      email: user.email,
+    const user = await User.create({
+        username,
+        firstname,
+        lastname,
+        email,
+        password,
     });
-  } else {
-    res.status(400);
-    throw new Error('Invalid user data');
-  }
+
+    user.save();
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 // @desc Logout user / clear cookie
 // @route POST /api/users/logout
 // @access Public
 const logoutUser = (req, res) => {
-  res.cookie('jwt', '', {
-    httpOnly: true,
-    expires: new Date(0),
-    sameSite: 'None',
-    secure: true,
-  });
-  res.status(200).json({ message: 'Logged out successfully' });
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+        sameSite: 'None',
+        secure: true,
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
 };
 
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
+    const user = await User.findById(req.user._id);
 
-  if (user) {
-    res.json({
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-    });
-  } else {
-    res.status(404);
-    throw new Error('User not found');
-  }
+    if (user) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc Update user profile
 // @route PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
+    const user = await User.findById(req.user._id);
 
-  if (user) {
-    user.username = req.body.username || user.username;
-    user.firstname = req.body.firstname || user.firstname;
-    user.lastname = req.body.lastname || user.lastname;
-    user.email = req.body.email || user.email;
+    if (user) {
+        user.username = req.body.username || user.username;
+        user.firstname = req.body.firstname || user.firstname;
+        user.lastname = req.body.lastname || user.lastname;
+        user.email = req.body.email || user.email;
 
-    const updatedUser = await user.save();
+        const updatedUser = await user.save();
 
-    res.json({
-      _id: updatedUser._id,
-      username: user.username,
-      firstname: user.firstname,
-      lastname: user.lastname,
-      email: updatedUser.email,
-    });
-  } else {
-    res.status(404);
-    throw new Error('User not found');
-  }
+        res.json({
+            _id: updatedUser._id,
+            username: user.username,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            email: updatedUser.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc Get all users
 // @route GET /api/users
 // @access Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find({});
-  res.json(users);
+    const users = await User.find({});
+    res.json(users);
 });
 
 // @desc Delete user
 // @route DELETE /api/users/:id
 // access Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id);
 
-  if (user) {
-    await User.deleteOne({ _id: user._id });
-    res.json({ message: 'User removed' });
-  } else {
-    res.status(404);
-    throw new Error('User not found');
-  }
+    if (user) {
+        await User.deleteOne({ _id: user._id });
+        res.json({ message: 'User removed' });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc Get user by ID
 // @route GET /api/users/:id
 // @access Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id).select('-password');
+    const user = await User.findById(req.params.id).select('-password');
 
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404);
-    throw new Error('User not found');
-  }
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc    Update user
 // @route   PUT /api/users/:id
 // @access  Private
 const updateUser = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id);
 
-  if (user) {
-    user.username = req.body.username || user.username;
-    user.firstname = req.body.firstname || user.firstname;
-    user.lastname = req.body.lastname || user.lastname;
-    user.email = req.body.email || user.email;
+    if (user) {
+        user.username = req.body.username || user.username;
+        user.firstname = req.body.firstname || user.firstname;
+        user.lastname = req.body.lastname || user.lastname;
+        user.email = req.body.email || user.email;
 
-    if (req.body.password) {
-      user.password = req.body.password;
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            firstname: updatedUser.username,
+            firstname: updatedUser.lastname,
+            email: updatedUser.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
     }
-
-    const updatedUser = await user.save();
-
-    res.json({
-      _id: updatedUser._id,
-      username: updatedUser.username,
-      firstname: updatedUser.username,
-      firstname: updatedUser.lastname,
-      email: updatedUser.email,
-    });
-  } else {
-    res.status(404);
-    throw new Error('User not found');
-  }
 });
 
 module.exports = {
-  authUser,
-  registerUser,
-  logoutUser,
-  getUserProfile,
-  updateUserProfile,
-  getUsers,
-  deleteUser,
-  getUserById,
-  updateUser,
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser,
 };
--- back-end/test/controllers/userController.test.js
+++ back-end/test/controllers/userController.test.js
@@ -11,121 +11,121 @@ const expect = chai.expect;
 const agent = chai.request.agent(app);
 
 describe('User Controller', () => {
-  beforeEach(async () => {
-    await Users.deleteMany();
-  });
+    beforeEach(async () => {
+        await Users.deleteMany();
+    });
 
-  // registerUser - return status 201
-  it('Register a new user - Successful', async () => {
-    const reqBody = {
-      username: 'testuser',
-      firstname: 'Firstname',
-      lastname: 'Lastname',
-      email: 'testemail@gmail.com',
-      password: '123456789',
-    };
-    //
-    const res = await agent.post('/api/users').send(reqBody);
-    expect(res).to.have.status(201);
-  });
+    // registerUser - return status 201
+    it('Register a new user - Successful', async () => {
+        const reqBody = {
+            username: 'testuser',
+            firstname: 'Firstname',
+            lastname: 'Lastname',
+            email: 'testemail@gmail.com',
+            password: '123456789',
+        };
+        //
+        const res = await agent.post('/api/users').send(reqBody);
+        expect(res).to.have.status(201);
+    });
 
-  // registerUser - return status 500
-  it('Register a new user - Duplicate key username', async () => {
-    await registerTestUser();
+    // registerUser - return status 500
+    it('Register a new user - Duplicate key username', async () => {
+        await registerTestUser();
 
-    // re-register a user with existing username
-    const reqBodyUser = {
-      username: 'testuser',
-      firstname: 'Firstname',
-      lastname: 'Lastname',
-      email: 'newtestuser@gmail.com',
-      password: '123456789',
-    };
-    const res = await agent.post('/api/users').send(reqBodyUser);
-    expect(res).to.have.status(500);
-  });
+        // re-register a user with existing username
+        const reqBodyUser = {
+            username: 'testuser',
+            firstname: 'Firstname',
+            lastname: 'Lastname',
+            email: 'newtestuser@gmail.com',
+            password: '123456789',
+        };
+        const res = await agent.post('/api/users').send(reqBodyUser);
+        expect(res).to.have.status(500);
+    });
 
-  // registerUser - return status 400
-  it('Register a new user - User email already exists', async () => {
-    await registerTestUser();
+    // registerUser - return status 400
+    it('Register a new user - User email already exists', async () => {
+        await registerTestUser();
 
-    // re-register a user with existing username
-    const reqBodyUser = {
-      username: 'newtestuser',
-      firstname: 'Firstname',
-      lastname: 'Lastname',
-      email: 'testemail@gmail.com',
-      password: '123456789',
-    };
-    const res = await agent.post('/api/users').send(reqBodyUser);
-    expect(res).to.have.status(400);
-  });
+        // re-register a user with existing username
+        const reqBodyUser = {
+            username: 'newtestuser',
+            firstname: 'Firstname',
+            lastname: 'Lastname',
+            email: 'testemail@gmail.com',
+            password: '123456789',
+        };
+        const res = await agent.post('/api/users').send(reqBodyUser);
+        expect(res).to.have.status(400);
+    });
 
-  // authUser - return status 200
-  it('Auth user - Successful', async () => {
-    await registerTestUser();
-    const res = await loginTestUser();
-    expect(res).to.have.status(200);
-    expect(res).to.have.cookie('jwt');
-  });
+    // authUser - return status 200
+    it('Auth user - Successful', async () => {
+        await registerTestUser();
+        const res = await loginTestUser();
+        expect(res).to.have.status(200);
+        expect(res).to.have.cookie('jwt');
+    });
 
-  // authUser - return status 401
-  it('Auth user - Invalid email', async () => {
-    await registerTestUser();
+    // authUser - return status 401
+    it('Auth user - Invalid email', async () => {
+        await registerTestUser();
 
-    // Auth user with invalid email
-    const reqBody = {
-      email: 'invalidemail@gmail.com',
-      password: '123456789',
-    };
-    const res = await agent.post('/api/users/auth').send(reqBody);
-    expect(res).to.have.status(401);
-  });
+        // Auth user with invalid email
+        const reqBody = {
+            email: 'invalidemail@gmail.com',
+            password: '123456789',
+        };
+        const res = await agent.post('/api/users/auth').send(reqBody);
+        expect(res).to.have.status(401);
+    });
 
-  // authUser - return status 401
-  it('Auth user - Invalid password', async () => {
-    await registerTestUser();
+    // authUser - return status 401
+    it('Auth user - Invalid password', async () => {
+        await registerTestUser();
 
-    // Auth user with invalid password
-    const reqBody = {
-      email: 'testemail@gmail.com',
-      password: 'invalidpassword',
-    };
-    const res = await agent.post('/api/users/auth').send(reqBody);
-    expect(res).to.have.status(401);
-  });
+        // Auth user with invalid password
+        const reqBody = {
+            email: 'testemail@gmail.com',
+            password: 'invalidpassword',
+        };
+        const res = await agent.post('/api/users/auth').send(reqBody);
+        expect(res).to.have.status(401);
+    });
 
-  // Logout - return status 200
-  it('Logout user - Successful', async () => {
-    await registerTestUser();
-    const cookies = (await loginTestUser()).headers['set-cookie'];
-    const res = await agent
-      .post('/api/users/logout')
-      .set('Cookie', cookies)
-      .send();
-    expect(res).to.have.status(200);
-  });
+    // Logout - return status 200
+    it('Logout user - Successful', async () => {
+        await registerTestUser();
+        const cookies = (await loginTestUser()).headers['set-cookie'];
+        const res = await agent
+            .post('/api/users/logout')
+            .set('Cookie', cookies)
+            .send();
+        expect(res).to.have.status(200);
+    });
 });
 
 after(() => {
-  agent.close();
+    agent.close();
 });
 
 const registerTestUser = () => {
-  const reqBody = {
-    username: 'testuser',
-    firstname: 'Firstname',
-    lastname: 'Lastname',
-    email: 'testemail@gmail.com',
-    password: '123456789',
-  };
-  return agent.post('/api/users').send(reqBody);
+    const reqBody = {
+        username: 'testuser',
+        firstname: 'Firstname',
+        lastname: 'Lastname',
+        email: 'testemail@gmail.com',
+        password: '123456789',
+    };
+    return agent.post('/api/users').send(reqBody);
 };
 
 const loginTestUser = () => {
-  const reqBody = {
-    email: 'testemail@gmail.com',
-    password: '123456789',
-  };
-  return agent.post('/api/users/auth').send(reqBody);
+    const reqBody = {
+        email: 'testemail@gmail.com',
+        password: '123456789',
+    };
+    return agent.post('/api/users/auth').send(reqBody);
 };
