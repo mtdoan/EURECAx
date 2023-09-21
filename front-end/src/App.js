32c37f751055711d3340e4614adaa9725efaf4a9
paul-mcn <paul.a.mcnamara@outlook.com>
2023-09-21 17:03:33 +1000

    fixed merge

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,11 +5,6 @@ import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
 import DashboardFlow from "components/DashboardFlow";
-import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import "@fontsource/inter";
-import Dashboard from "./components/dashboard/Dashboard";
-import SignIn from "components/auth/SignIn";
 
 function App() {
 	global.route = "http://localhost:3000";
