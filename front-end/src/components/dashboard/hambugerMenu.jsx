55f00d61796f349d3bff3e7ccf7cff02c79ed96f
ahmyk9 <ahmedkhilji99@gmail.com>
2023-09-22 19:09:52 +1000

    resizing Sidebar added

--- front-end/src/components/dashboard/hambugerMenu.jsx
+++ front-end/src/components/dashboard/hambugerMenu.jsx
@@ -1,7 +1,8 @@
 import './hamburgerMenu.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
-
-
+//Assets 
 import Choas1Icon from './assets/choas1Icon'
 import Choas1IconFull from './assets/choas1__logo--full'
 import ConnectIcon from './assets/connectIcon'
@@ -9,11 +10,6 @@ import DashboardIcon from './assets/dashboardIcon'
 import ResizeMenuButton from './assets/resizeMenuButton'
 import DocumentsIcon from './assets/documentsIcon'
 import LearnIcon from './assets/learnIcon'
-
-
-import { useNavigate } from 'react-router-dom'
-
-// import { useNavigate } from "react-router-dom";
 
 
 
@@ -39,55 +35,117 @@ const HamburgerMenu = () => {
 	// 		navigate("./");
 	// }
 
+
+
+
 		
-    
+	document.addEventListener("click", function() {
+		const resizeButton = document.getElementById("resizeButton");
+
+			
+		resizeButton.addEventListener("click", function() {
+			const sideBarSize = document.querySelectorAll(".menu");
+			const menuItem= document.querySelectorAll(".menu__item");
+			const menuItemName = document.querySelectorAll(".menu__item--name");
+			const resizeButton = document.querySelectorAll(".resizeButton");
+
+
+			sideBarSize.forEach(function(element) {
+				element.classList.toggle("collapsed__menu");
+			});
+
+			  
+			menuItem.forEach(function(element) {
+			element.classList.toggle("menu__item--after");
+			});
+
+			menuItemName.forEach(function(element) {
+			element.classList.toggle("menu__item--name--remove");
+			  });
+
+			resizeButton.forEach(function(element) {
+			element.classList.toggle("resizeButton--after");
+			  });
+
+		});
+			
+		});
+
+
+
+
+	  
+
+	// Get references to the button and the target element
+
 
 
 	return (
 		<>
-			<div className="w-1/4 h-full">
-
-				
+			<div  id="menu" className="menu" >
 
 					<div className="logo__container">
 						<figure className="logo__container--holder">
-							<Choas1IconFull />
+							<Choas1IconFull id="logo" className="full__logo" />
 						</figure>
+						<button id="resizeButton" className="resizeButton--container" > 
+							<ResizeMenuButton className="resizeButton" />
+						</button>
 					</div>
 
-					<button className="resizeButton--container" id="expand-button"> 
-						<ResizeMenuButton/>
-					</button>
 
 				<div className="container">
 
-
-
-
 					<div className="menu__item--container">
-							<div className='menu__item'
+							<div 
+							className='menu__item'
 							type="button"
 							onClick={(e) => openPage(e)}
 							>
 										<figure className="menu__item--svg-container">
 											<DashboardIcon className="menu__item--svg" />
 										</figure>
-
 										<p className="menu__item--name">Dashboard</p>
 							</div>
 					</div>
 					<div className="menu__item--container">
-							<div className='menu__item'
+							<div 
+							className='menu__item'
+							type="button"
+							onClick={(e) => openPage(e)}
+							>
+										<figure className="menu__item--svg-container">
+											<DocumentsIcon className="menu__item--svg" />
+										</figure>
+										<p className="menu__item--name">Documents</p>
+							</div>
+					</div>
+					<div className="menu__item--container">
+							<div 
+							className='menu__item'
 							type="button"
 							onClick={(e) => openPage(e)}
 							>
 										<figure className="menu__item--svg-container">
 											<ConnectIcon className="menu__item--svg" />
 										</figure>
-
 										<p className="menu__item--name">Connect</p>
 							</div>
 					</div>
+					<div className="menu__item--container">
+							<div 
+							className='menu__item'
+							type="button"
+							onClick={(e) => openPage(e)}
+							>
+										<figure className="menu__item--svg-container">
+											<LearnIcon className="menu__item--svg" />
+										</figure>
+										<p className="menu__item--name">Learn</p>
+							</div>
+					</div>
+ 
+
 
 
 
@@ -97,6 +155,8 @@ const HamburgerMenu = () => {
 		</>
 	)
 }
+
+
 
 export default HamburgerMenu
 
