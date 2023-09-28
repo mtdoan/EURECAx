d68c2cb3cac32bdae8f4ed4dd80f158694308493
ahmyk9 <ahmedkhilji99@gmail.com>
2023-09-28 15:55:06 +1000

    fixed bugs

--- front-end/src/components/dashboard/hambugerMenu.jsx
+++ front-end/src/components/dashboard/hambugerMenu.jsx
@@ -1,6 +1,7 @@
 import './hamburgerMenu.css'
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
 //Assets 
 import Choas1Icon from './assets/choas1Icon'
@@ -15,136 +16,106 @@ import LearnIcon from './assets/learnIcon'
 
 
 const HamburgerMenu = () => {
-	let navigate = useNavigate()
 
 	const [showFullLogo, setShowFullLogo] = useState(true); // State variable to track the logo icon
 
-	const toggleLogo = () => {
+	const resizeMenu = () => {
 	  setShowFullLogo(!showFullLogo); // Toggle between full and short logo
+	  const sideBarSize = document.querySelectorAll(".menu");
+	  const menuItem = document.querySelectorAll(".menu__item");
+	  const menuItemName = document.querySelectorAll(".menu__item--name");
+	  const resizeButton = document.querySelectorAll(".resizeButton");
+  
+	  const toggleClasses = function (elements, className) {
+		for (let i = 0; i < elements.length; i++) {
+		  elements[i].classList.toggle(className);
+		}
+	  };
+  
+	  toggleClasses(sideBarSize, "collapsed__menu");
+	  toggleClasses(menuItem, "menu__item--after");
+	  toggleClasses(menuItemName, "menu__item--name--remove");
+	  toggleClasses(resizeButton, "resizeButton--after");
 	}
 
-
-		const openPage = async () => {
-
-			const menuItemName = document.getElementsByClassName("menu__item--name");
-			let menuItemNames = ['Dashboard', 'Connect', 'Learn', 'Documents' ] // Names of every menu item
-
-			for (let i = 0; i < menuItemNames.length; i++){
-				if ( menuItemName[0].innerHTML == menuItemNames[i]){
-						console.log('Button Works!')
-						navigate("./");
-				}
-			}
-	}
-
-
-	// For Loop Version - testing
-
-	document.addEventListener("click", function () {
-		const resizeButton = document.getElementById("resizeButton");
-		resizeButton.addEventListener("click", function () {
-
-
-
-		  const sideBarSize = document.querySelectorAll(".menu");
-		  const menuItem = document.querySelectorAll(".menu__item");
-		  const menuItemName = document.querySelectorAll(".menu__item--name");
-		  const resizeButton = document.querySelectorAll(".resizeButton");
-	  
-		  const toggleClasses = function (elements, className) {
-			for (let i = 0; i < elements.length; i++) {
-			  elements[i].classList.toggle(className);
-			}
-		  };
-	  
-		  toggleClasses(sideBarSize, "collapsed__menu");
-		  toggleClasses(menuItem, "menu__item--after");
-		  toggleClasses(menuItemName, "menu__item--name--remove");
-		  toggleClasses(resizeButton, "resizeButton--after");
-		});
-	  });
-	  
-
-	// document.addEventListener("click", function() {
-	// 	const resizeButton = document.getElementById("resizeButton");
-	// 	resizeButton.addEventListener("click", function() {
-
-	// 		const sideBarSize = document.querySelectorAll(".menu");
-	// 		sideBarSize.forEach(function(element) {
-	// 			element.classList.toggle("collapsed__menu");
-	// 		});
-			
-	// 		const menuItem= document.querySelectorAll(".menu__item");
-	// 		menuItem.forEach(function(element) {
-	// 			element.classList.toggle("menu__item--after");
-	// 		});
-			
-	// 		const menuItemName = document.querySelectorAll(".menu__item--name");
-	// 		menuItemName.forEach(function(element) {
-	// 			element.classList.toggle("menu__item--name--remove");
-	// 		});
-			
-	// 		const resizeButton = document.querySelectorAll(".resizeButton");
-	// 		resizeButton.forEach(function(element) {
-	// 		element.classList.toggle("resizeButton--after");
-	// 		  });
-
-
-
-	// 	});
-			
-	// });
-
-
-		
-		
 
 	return (
 		<>
 			<div  id="menu" className="menu" >
 
-					<div className="logo__container">
-						<figure className="logo__container--holder">
+					<div className="containers">
+						<div className="logo__container">
 							{showFullLogo ? <Choas1IconFull id="logo" className="full__logo" /> : <Choas1Icon />}
-						</figure>
+						</div>
 
-						<div className="resizeButton--container">
-
-							<button id="resizeButton" onClick={toggleLogo}> 
+			
+							<button id="resizeButton" onClick={resizeMenu}> 
 								<ResizeMenuButton className="resizeButton" />
 							</button>
-						</div>
 					</div>
 
 
 				<div className="container">
-
-					<div className="menu__item--container">
-							<div 
-							className='menu__item'
-							type="button"
-							onClick={() => openPage()}
-							>
-										<figure className="menu__item--svg-container">
-											<DashboardIcon className="menu__item--svg" />
-										</figure>
-										<p className="menu__item--name">Dashboard</p>
-							</div>
-					</div>
-					<div className="menu__item--container">
-							<div 
-							className='menu__item'
-							type="button"
-							onClick={() => openPage()}
-							>
-										<figure className="menu__item--svg-container">
-											<DashboardIcon className="menu__item--svg" />
-										</figure>
-										<p className="menu__item--name">Connect</p>
-							</div>
-					</div>
-					
-
+					<Link to="/dashboard/new-project">
+						<div className="menu__item--container">
+								<div 
+								className='menu__item'
+								type="button"
+								>
+											<figure className="menu__item--svg-container">
+												<DashboardIcon className="menu__item--svg" />
+											</figure>
+											<div className="menu__item--name-container">
+												<p className="menu__item--name">Dashboard</p>
+											</div>
+								</div>
+						</div>
+					</Link>
+					<Link to="/dashboard/new-project">
+						<div className="menu__item--container">
+								<div 
+								className='menu__item'
+								type="button"
+								>
+											<figure className="menu__item--svg-container">
+												<DocumentsIcon className="menu__item--svg" />
+											</figure>
+											<div className="menu__item--name-container">
+												<p className="menu__item--name">Documents</p>
+											</div>
+								</div>
+						</div>
+					</Link>
+					<Link to="/dashboard/new-project">
+						<div className="menu__item--container">
+								<div 
+								className='menu__item'
+								type="button"
+								>
+											<figure className="menu__item--svg-container">
+												<ConnectIcon className="menu__item--svg" />
+											</figure>
+											<div className="menu__item--name-container">
+												<p className="menu__item--name">Connect</p>
+											</div>
+								</div>
+						</div>
+					</Link>
+					<Link to="/dashboard/new-project">
+						<div className="menu__item--container">
+								<div 
+								className='menu__item'
+								type="button"
+								>
+											<figure className="menu__item--svg-container">
+												<LearnIcon className="menu__item--svg" />
+											</figure>
+											<div className="menu__item--name-container">
+												<p className="menu__item--name">Learn</p>
+											</div>
+								</div>
+						</div>
+					</Link>
 
 				</div>  
 				{/* ^^^^this is container Div */}
