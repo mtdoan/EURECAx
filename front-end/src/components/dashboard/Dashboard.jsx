a98fae112d3898a826b8c35a74111e82d32f603b
ahmyk9 <ahmedkhilji99@gmail.com>
2023-09-28 17:51:41 +1000

    fixed sidebar/header overlap

--- front-end/src/components/dashboard/Dashboard.jsx
+++ front-end/src/components/dashboard/Dashboard.jsx
@@ -33,25 +33,32 @@ export default function Dashboard() {
     };
 
     return (
-        <>
-            <div className="App">
-                <Header />
-                <div className="flex flex-row h-[90vh] z-0">
-                    <HamburgerMenu />
-                    <div className="w-full h-full bg-gray-100">
-                        {/** <Dashboard /> */}
-                    </div>
-                    <div>
-                        <button
-                            type="signout"
-                            onClick={() => logout()}
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Sign out
-                        </button>
-                    </div>
-                </div>
+<>
+      <div className="App">
+        <div className="flex flex-row w-full">
+          <HamburgerMenu />
+          <div className="w-full border-l-2 ">
+            <div className="[&_nav]:flex [&_nav]:flex-row h-[10vh] flex flex-row items-center w-full justify-center border-b-2">
+              {/* <NavBar /> */}
             </div>
-        </>
+            <div className="flex flex-row h-[90vh] z-0">
+                {/* <Outlet /> */}
+              {/**
+                This is just gonna be hidden for the demo
+              <div>
+                <button
+                  type="signout"
+                  onClick={() => logout()}
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign out
+                </button>
+              </div>
+              */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
     )
 }
