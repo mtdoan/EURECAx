808b6a473cea6bd7e78c007fc9b071073166b5ff
paul-mcn <paul.a.mcnamara@outlook.com>
2023-10-13 18:49:51 +1100

    added tooltip

--- front-end/src/components/shared/index.js
+++ front-end/src/components/shared/index.js
@@ -1,4 +1,5 @@
 import ExportJsonLink from "./ExportJsonLink";
 import LinkButton from "./LinkButton";
+import Tooltip from "./Tooltip";
 
-export { ExportJsonLink, LinkButton };
+export { ExportJsonLink, LinkButton, Tooltip };
