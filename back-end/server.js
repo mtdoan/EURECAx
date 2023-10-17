34bf0fb8ae9fbcc7f42af507f785a82e923271f4
Mindy Doan <mtdoan0109@gmail.com>
2023-10-17 22:27:57 +1100

    historical search suggestion

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require('./routes/userRoutes');
+const usageRoutes = require('./routes/usageRoutes');
 
 const User = require('./models/userModel');
 
@@ -12,24 +13,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.enable("trust proxy")
+app.enable('trust proxy');
 app.use(
     cors({
         origin: true,
         credentials: true,
-        exposedHeaders: ["jwt", "set-cookie", "Set-Cookie"],
+        exposedHeaders: ['jwt', 'set-cookie', 'Set-Cookie'],
     })
 );
 
-
-app.get("/", (req, res) => {
-    res.json({ message: "v7" });
+app.get('/', (req, res) => {
+    res.json({ message: 'v7' });
 });
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello World!" });
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello World!' });
 });
 
-app.use("/api/users", userRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/usages', usageRoutes);
 
 module.exports = { app };
